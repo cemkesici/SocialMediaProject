6b958430652aca6133d2a2bbbff4ff868fe31561
Cem Kesici <cemkesici51@gmail.com>
2023-06-24 18:03:24 +0300

    Giriş yapmak ve giriş yapılan hesabı hatırlamak için kodlar

--- app/src/main/java/com/cem/socialmediaproject/LoginActivity.kt
+++ app/src/main/java/com/cem/socialmediaproject/LoginActivity.kt
@@ -1,12 +1,11 @@
 package com.cem.socialmediaproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -23,11 +22,33 @@ class LoginActivity : AppCompatActivity() {
 
         mail=findViewById(R.id.MailText)
         pass=findViewById(R.id.PasswordText)
+
+        val kullanici=auth.currentUser// güncel kullanıcı bilgisi
+        if(kullanici!= null){// güncel kullanıcı varsa giriş yapılmıştır ve tekrar login sayfasını açmadan anasayfaya atmamız lazım
+            val intent=Intent(this, TimeLineActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
-    fun logIn(view: View){
+    fun logIn(view: View){//giriş yapmak için
+        val email=mail.text.toString()
+        val password=pass.text.toString()
 
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task->
+            if (task.isSuccessful){
+                val kullanici=auth.currentUser?.email.toString()// güncel kullanıcı bilgisi
+                Toast.makeText(this,"Hoşgeldiniz $kullanici", Toast.LENGTH_LONG).show()
+
+                val intent=Intent(this,TimeLineActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener{exception->
+            Toast.makeText(this,exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
     }
+
     fun signIn(view:View){
         val email=mail.text.toString()
         val password=pass.text.toString()
