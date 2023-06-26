bcbc148c28cb0b236ec3145f9a272912f97170c1
Cem Kesici <cemkesici51@gmail.com>
2023-06-26 16:06:59 +0300

    Görsel seçme ve kullanıcı izinleri alındı

--- app/src/main/java/com/cem/socialmediaproject/FotografPaylasmaActivity.kt
+++ app/src/main/java/com/cem/socialmediaproject/FotografPaylasmaActivity.kt
@@ -1,11 +1,74 @@
 package com.cem.socialmediaproject
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class FotografPaylasmaActivity : AppCompatActivity() {
+    private var secilenGorsel:Uri?=null
+    private var gorselBitmap:Bitmap?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fotograf_paylasma)
+    }
+
+    fun paylas(view: View){
+
+    }
+    @Suppress("DEPRECATION")
+    fun gorselSec(view:View){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            //İZİN ALINMAMIŞSA
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),0)
+        }else {
+            val galeriIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galeriIntent,1)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode==0){
+            if (grantResults.isNotEmpty() &&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                val galeriIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,1)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @Deprecated("Deprecated in Java")
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val ImageView=findViewById<ImageView>(R.id.imageView2)
+        if(requestCode==1 && resultCode==Activity.RESULT_OK && data!=null){
+           secilenGorsel= data.data
+            if(secilenGorsel!=null){
+                if (Build.VERSION.SDK_INT>=28){
+                    val source=ImageDecoder.createSource(this.contentResolver,secilenGorsel!!)
+                    gorselBitmap=ImageDecoder.decodeBitmap(source)
+                    ImageView.setImageBitmap(gorselBitmap)
+                }else {
+                    gorselBitmap =MediaStore.Images.Media.getBitmap(this.contentResolver, secilenGorsel)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
