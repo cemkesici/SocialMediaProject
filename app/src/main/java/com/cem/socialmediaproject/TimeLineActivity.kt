50c31521ece6a359ea4139e7a993c86092842ed4
Cem Kesici <cemkesici51@gmail.com>
2023-06-26 14:45:26 +0300

    Çıkış yapma işlemi ve fotoğraf paylaşmak için activity eklendi

--- app/src/main/java/com/cem/socialmediaproject/TimeLineActivity.kt
+++ app/src/main/java/com/cem/socialmediaproject/TimeLineActivity.kt
@@ -1,11 +1,39 @@
 package com.cem.socialmediaproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class TimeLineActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_line)
+        auth= FirebaseAuth.getInstance()
+    }
+    @Suppress("UsePropertyAccessSyntax")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater=getMenuInflater()// menuyu çağırdık
+        menuInflater.inflate(R.menu.secenekler_menu,menu)// menuyu ınflate ettik
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.fotograf_paylas){
+            //fotoğraf paylaşma aktivetisine gidecek
+            val Intent=Intent(this,FotografPaylasmaActivity::class.java)
+            startActivity(Intent)
+        }
+        else if(item.itemId==R.id.cikis){
+            auth.signOut()//firebase çıkış işlemi
+
+            val Intent= Intent(this,LoginActivity::class.java)//login sayfasını çağırıypruz
+            startActivity(Intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
